0c79ec9d653383a73aced16715a6f8e27bfb56b9
sarathkumar-optisol <sarathkumar.s@optisolbusiness.com>
2021-05-05 12:01:49 +0530

    Save images,videos,docs in storage

---
+++ app/src/main/java/com/droid/storeallfiles/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.droid.storeallfiles
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.Toast
+import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import kotlin.concurrent.thread
+
+const val PICK_FILE = 1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnSelect = findViewById<Button>(R.id.btnSelect)
+
+        btnSelect.setOnClickListener {
+            pickFileAndCopyUriToExternalFilesDir()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            PICK_FILE -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val uri = data.data
+                    if (uri != null) {
+                        val fileName = getFileNameByUri(uri)
+                        copyUriToExternalFilesDir(uri, fileName)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun pickFileAndCopyUriToExternalFilesDir() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "*/*"
+        startActivityForResult(intent, PICK_FILE)
+    }
+
+    private fun getFileNameByUri(uri: Uri): String {
+        var fileName = System.currentTimeMillis().toString()
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        if (cursor != null && cursor.count > 0) {
+            cursor.moveToFirst()
+            fileName = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME))
+            cursor.close()
+        }
+        return fileName
+    }
+
+    private fun  copyUriToExternalFilesDir(uri: Uri, fileName: String) {
+        thread {
+            val inputStream = contentResolver.openInputStream(uri)
+            val tempDir = getExternalFilesDir("temp")
+            if (inputStream != null && tempDir != null) {
+                val file = File("$tempDir/$fileName")
+                val fos = FileOutputStream(file)
+                val bis = BufferedInputStream(inputStream)
+                val bos = BufferedOutputStream(fos)
+                val byteArray = ByteArray(1024)
+                var bytes = bis.read(byteArray)
+                while (bytes > 0) {
+                    bos.write(byteArray, 0, bytes)
+                    bos.flush()
+                    bytes = bis.read(byteArray)
+                }
+                bos.close()
+                fos.close()
+                runOnUiThread {
+                    Toast.makeText(this, "Copy file into $tempDir succeeded.", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+}
